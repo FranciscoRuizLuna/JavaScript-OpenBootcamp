f27179d8357a3bbbe99a4ffb2bb0cdc4ad3f8ff1
Francisco Ruiz <franciscoruizluna@hotmail.com>
2023-03-14 14:38:01 -0600

    Realizando ejercicio 2

---
+++ Ejercicio2/ejercicio2.js
@@ -0,0 +1,12 @@
+const fecha = new Date("September 04 1999");
+const libro = {
+  titulo: "Biblia",
+  autor: "Dios",
+  fecha: "Eternidad",
+  url: "https://www.bible.com/es/"
+}
+const lista = ["Francisco Ruiz", 23, true, fecha, libro];
+
+
+
+console.log(lista);
